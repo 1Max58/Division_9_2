551db5e2fb9515d5657d11140f3947e12c7d4f33
1Max58 <>
2023-12-14 22:18:55 +0200

    ArithmeticException case, ArrayIndexOutOfBoundsException case and no exception thrown case was implemented

---
+++ src/com/example/division/Division.java
@@ -0,0 +1,7 @@
+package com.example.division;
+
+public class Division {
+    public static int divideByArrayIndex(int dividend, int[] divisors, int divisorIndex) {
+        return dividend / divisors[divisorIndex];
+    }
+}
--- src/com/example/division/Main.java
+++ src/com/example/division/Main.java
@@ -2,6 +2,28 @@ package com.example.division;
 
 public class Main {
     public static void main(String[] args) {
-
+        int dividend = 8;
+        int[] divisors = new int[] { 2, 0 };
+        int arithmeticExceptionDivisorIndex = 1;
+        int arrayIndexOutOfBoundsExceptionDivisorIndex = 2;
+        int notThrowAnyExceptionDivisorIndex = 0;
+        try {
+            try {
+                Division.divideByArrayIndex(dividend, divisors, arithmeticExceptionDivisorIndex);
+            } catch (ArithmeticException e) {
+                System.out.printf("Exception was thrown for index %d:%n", arithmeticExceptionDivisorIndex);
+                System.out.println(e);
+            }
+            try {
+                Division.divideByArrayIndex(dividend, divisors, arrayIndexOutOfBoundsExceptionDivisorIndex);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.printf("Exception was thrown for index %d:%n", arrayIndexOutOfBoundsExceptionDivisorIndex);
+                System.out.println(e);
+            }
+            Division.divideByArrayIndex(dividend, divisors, notThrowAnyExceptionDivisorIndex);
+            System.out.printf("No exception was thrown for index %d%n", notThrowAnyExceptionDivisorIndex);
+        } finally {
+            System.out.println("Finally block happened");
+        }
     }
 }
